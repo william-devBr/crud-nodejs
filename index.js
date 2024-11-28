be811b5dbb0ce9002df38e1b61040932df2d119a
WILLIAM BATISTA <53185623+william-devBr@users.noreply.github.com>
2024-11-28 15:45:15 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+const db = require('./utils/db')
+
+const PORT = process.env.PORT || 3000
+
+app.use(bodyPaser.urlencode({extended: true}))
+
+app.get('/users/:id?',(req,res) => {
+  
+ //rota para listar os usuários cadastrados
+
+  const params = {};
+
+   if(req.params.id) {
+        // se existir query id na URL atribui ao @params
+       params.id = req.params.id;
+   }
+  db.select('users', params,(err,results)=> {
+        if(err) throw console.log(`ocorreu um erro ${err}`);
+
+         // responde a busca com dados em JSON
+        res.json(results);
+       
+  })
+
+});
+
+app.post('/create-user',(req,res)=> {
+
+     const {nome,email} = req.body; 
+    if(!email.length && !nome.length) throw console.log('preencha os campos \"nome\" e \"email\" corretamente !');
+
+   db.insert('users',{nome : nome, email : email},(err, results) => {
+          if(!err) {
+            res.send(`<h3>cadastrado com sucesso !</h3>`);
+          }else {
+            console.log('ocorreu um erro ao efetuar o cadastro, tente novamente');
+          }
+   })
+})
+
+app.listen(PORT,'localhost',()=> {
+  console.log(`servior rodando na porta ${PORT}`)
+})
